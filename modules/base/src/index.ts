59f7af4881ffcb3273e6db6bedb080a8deb12718
Julian <gitea+julian@ic.thejulian.uk>
2023-05-09 12:10:19 +0200

    added pronouns-base for the absolute basics

---
+++ modules/base/src/index.ts
@@ -0,0 +1,5 @@
+import {Language} from "./Language";
+import {UserNotFoundError, NoDataBecauseUserNotFound} from "./Errors/UserNotFoundError";
+import {LanguageNotYetSupportedError} from "./Errors/LanguageNotYetSupportedError";
+
+export {Language, LanguageNotYetSupportedError, UserNotFoundError, NoDataBecauseUserNotFound};
---
+++ modules/base/webpack.node.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const { ModuleFederationPlugin } = require('webpack').container;
+module.exports = {
+  entry: {
+    index: {
+      import: './src/index.ts',
+    },
+  },
+  mode: 'production',
+  target: ['node'],
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'pronounsBase.js',
+    library: "pronounsBase",
+    libraryTarget: "umd",
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    globalObject: "this",
+    umdNamedDefine: true
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      runtime: 'show-your-pronouns',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /tests/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        type: 'asset/inline',
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  }
+};
